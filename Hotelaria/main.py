87dd1bf699e0cd777c061e84b436e62d72d1a6d1
Annaaa <140774860+srtaayanamy@users.noreply.github.com>
2024-08-04 11:54:58 -0300

    Update main.py

--- Hotelaria/main.py
+++ Hotelaria/main.py
@@ -1,6 +1,8 @@
 '''Sistema de hotelaria'''
 #adicionar reservas, cancelar reservas, buscar reservas pelo nome do hóspede e listar todas as reservas. 
 
+#vou ter que limitar o numero de quartos se n, n vou conseguir criar uma lógica pra n permitir reservas repetidas
+
 def adc_reserva(reservas, id, nome, numero, checkin, checkout):
         reserva = {
             'id' : id,
@@ -11,8 +13,22 @@ def adc_reserva(reservas, id, nome, numero, checkin, checkout):
         }
         reservas.append(reserva)
 
-def can_reservas():
-    pass
+def can_reservas(reservas, nome):
+    for reserva in reservas:
+        if reserva['nome'] == nome:
+            reservas.remove(reserva)
+            print('\nReserva no nome de {} foi cancelada.\n' .format(nome))
+            return
+    else:
+        print('\nReserva não encontrada.\n')
+
+def bus_reservas(reservas, nome):
+    for reserva in reservas:
+        if reserva['nome'] == nome:
+            print('\nReserva no nome de {} foi encontrada.\n' .format(nome))
+            return
+    else:
+        print('\nReserva não encontrada.\n')
 
 def menu():
     return input('\n O QUE DESEJA FAZER?\n[1]Fazer reserva\n[2]Cancelar reserva\n[3]Buscar Reserva\n[4]Listar Reserva\n[5]Sair\n')
@@ -31,10 +47,12 @@ def main():
             print('\nReserva feita!\n')
             
         elif esc == '2':
-            pass
+            nome = input ('\nNome do cliente a ser cancelado: ')
+            can_reservas(reservas, nome)
 
         elif esc == '3':
-            pass
+            nome = input('\nQual nome deseja buscar? ')
+            bus_reservas(reservas, nome)
 
         elif esc == '4':
             pass

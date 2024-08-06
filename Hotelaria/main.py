850323599757a0ebaec3caebf1416aa22a731be5
Annaaa <140774860+srtaayanamy@users.noreply.github.com>
2024-08-05 23:14:00 -0300

    Update main.py

--- Hotelaria/main.py
+++ Hotelaria/main.py
@@ -1,9 +1,8 @@
 '''Sistema de hotelaria'''
-#adicionar reservas, cancelar reservas, buscar reservas pelo nome do hóspede e listar todas as reservas. 
-
-#vou ter que limitar o numero de quartos se n, n vou conseguir criar uma lógica pra n permitir reservas repetidas
+#adicionar reservas, cancelar reservas, buscar reservas pelo nome do hóspede e listar todas as reservas.
 
 def adc_reserva(reservas, id, nome, numero, checkin, checkout):
+        id += 1
         reserva = {
             'id' : id,
             'nome' : nome,
@@ -12,12 +11,13 @@ def adc_reserva(reservas, id, nome, numero, checkin, checkout):
             'checkout' : checkout
         }
         reservas.append(reserva)
+        return id
 
-def can_reservas(reservas, nome):
+def can_reservas(reservas, id):
     for reserva in reservas:
-        if reserva['nome'] == nome:
+        if reserva['id'] == id:
             reservas.remove(reserva)
-            print('\nReserva no nome de {} foi cancelada.\n' .format(nome))
+            print('\nReserva de número {} foi cancelada.\n' .format(id))
             return
     else:
         print('\nReserva não encontrada.\n')
@@ -30,34 +30,42 @@ def bus_reservas(reservas, nome):
     else:
         print('\nReserva não encontrada.\n')
 
+def lis_reservas(reservas):
+    for reserva in reservas:
+        print(reserva)
+
 def menu():
-    return input('\n O QUE DESEJA FAZER?\n[1]Fazer reserva\n[2]Cancelar reserva\n[3]Buscar Reserva\n[4]Listar Reserva\n[5]Sair\n')
+    return input('\n O QUE DESEJA FAZER?\n[1]Fazer reserva\n[2]Cancelar reserva\n[3]Buscar Reserva\n[4]Listar Reservas\n[5]Editar reserva\n[6]Sair\n')
 
 def main():
+    id = 0
     reservas = []
     while True:
         esc = menu()
         if esc == '1':
-            id = input ('Id da reserva: ')
             nome = input ('Nome do cliente: ')
             numero = input ('Número do quarto: ')
             checkin = input ('Data de entrada: ')
             checkout = input ('Data de saída: ')
-            adc_reserva(reservas, id, nome, numero, checkin, checkout)
-            print('\nReserva feita!\n')
+            id = (adc_reserva(reservas, id, nome, numero, checkin, checkout))
+            print('\nReserva de número {} feita!\n' .format(id))
             
         elif esc == '2':
-            nome = input ('\nNome do cliente a ser cancelado: ')
-            can_reservas(reservas, nome)
+            id = int(input('\nNº da reserva a ser cancelada: '))
+            can_reservas(reservas, id)
 
         elif esc == '3':
             nome = input('\nQual nome deseja buscar? ')
             bus_reservas(reservas, nome)
 
         elif esc == '4':
-            pass
+            print('\nReservas do hotel: \n')
+            lis_reservas(reservas)
 
         elif esc == '5':
+            pass
+
+        elif esc == '6':
             print('\nENCERRANDO SISTEMA...')
             break
 

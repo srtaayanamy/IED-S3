f6fc7270e2d471e9036d724f8d7564598f263a04
Annaaa <140774860+srtaayanamy@users.noreply.github.com>
2024-08-21 20:52:38 -0300

    Atualizar o main.py

--- Biblioteca/main.py
+++ Biblioteca/main.py
@@ -1,17 +1,12 @@
 '''SISTEMA BIBLIOTECA'''
-#ANNA JULIA DE SOUSA FÉLIX
 
 biblioteca = {}
 
-#declarando a biblioteca vazia pra depois ir preenchendo ela conforme a função abaixo:
-#na função de adicionar livros, primeiro se faz a verificação se o livro já está na biblioteca e depois o adiciona na mesma com valores de autor e gênero
 def adicionar_livro(biblioteca, titulo, autor, genero):
     if titulo not in biblioteca:
         biblioteca[titulo] = {}
     biblioteca[titulo]['autor'] = autor
-    biblioteca[titulo]['genero'] = genero
-
-#depois de muito sofrer e ler artigos sobre dicionários em python, descobri que não se pode chamar uma chave que nao existe ainda :) foi traumatizante... 
+    biblioteca[titulo]['genero'] = genero
 
 def remover_livro(biblioteca, titulo):
     if titulo in biblioteca:
@@ -19,31 +14,24 @@ def remover_livro(biblioteca, titulo):
         print('\nO livro foi removido.\n')
     else:
         print('\nO livro não foi encontrado.\n')
-
-#aqui nas duas funções, tanto de remover quanto de buscar é verificado se o livro está na biblioteca, e caso esteja, ele é excluído ou dá o retorno de que foi encontrado.
-
+
 def buscar_livro(biblioteca, titulo):
         if titulo in biblioteca:
             print('\nO livro foi encontrado.\n')
         else:
-            print('\nO livro não foi encontrado.\n')
-
-#em listar_livro tem a verificação se a biblioteca existe ou se está vazia e depois itera sobre cada titulo na biblioteca com o método keys, que é basicamente pra retornar a visualização das chaves no dicionário
+            print('\nO livro não foi encontrado.\n')
 
 def listar_livros(biblioteca, titulo):
     if biblioteca:
         for titulo in biblioteca.keys():
             print(titulo)
     else:
-        print('A biblioteca está vazia, adicione alguk livro.\n')
-
-#funções de menu com opções pro usuário escolher...
+        print('A biblioteca está vazia, adicione alguk livro.\n')
 
 def menu():
     return input('\nSISTEMA BIBLIOTECA\nEscolha a opção:\n [1]Adicionar livro na biblioteca\n [2]Remover livro da biblioteca\n [3]Buscar livro\n [4]Listar livros\n [5]Sair\n')
 
-def main():
-#def main com laço while true pra ficar repetindo e acionando todas as funções pelo esc que veio num retorno da menu()
+def main():
     while True:
         esc = menu()
         if esc == '1':    
@@ -55,8 +43,7 @@ def main():
         elif esc == '2':
             titulo = input('Qual exemplar deseja remover da biblioteca? ')
             remover_livro(biblioteca, titulo)
-            print('Lista de livros da biblioteca: ', biblioteca)
-            #queria muito tirar essas chaves do print, infelizmente fica pra proxima
+            print('Lista de livros da biblioteca: ', biblioteca)
 
         elif esc == '3':
             titulo = input('Qual livro deseja buscar? ')
@@ -71,9 +58,7 @@ def main():
             break
 
         else:
-            print('OPÇÃO INVÁLIDA! Escolha outra opção, por favor.\n')
-
-#módulo pra executar o código diretamente
+            print('OPÇÃO INVÁLIDA! Escolha outra opção, por favor.\n')
 
 if __name__ == '__main__':
     main()
